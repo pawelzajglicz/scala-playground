64a6e15fca3366f9ff52e02c795d839b4d5b6d59
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-12-25 22:36:26 +0100

    method notations

---
+++ rock-the-jvm-scala-beginners/src/lectures/part2oop/MethodNotations.scala
@@ -0,0 +1,48 @@
+package lectures.part2oop
+
+object MethodNotations extends App {
+
+  class Person(val name: String, favoriteMovie: String) {
+
+    def likes(movie: String): Boolean = movie == favoriteMovie
+    def hangOutWith(person: Person): String = s"$name is hanging out with ${person.name}"
+    def +(person: Person): String = s"$name is hanging out with ${person.name}"
+    def unary_! : String = s"$name, what the heck?!"
+    def isAlive: Boolean = true
+    def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+  }
+
+  val mary = new Person("Mary", "Inception")
+  println(mary.likes("Inception"))
+  println( mary likes "Inception")  // equivalent
+  // infix notation + operator notation --> works only with methods with single parameter
+  // syntactic sugar
+
+  // "operators" in Scala
+  val tom = new Person("Tom", "Fight Club")
+  println(mary hangOutWith tom)
+  println(mary + tom)
+  println(mary.+(tom))
+  println(1 + 2)
+  println(1.+(2))
+
+  // ALL OPERATORS ARE METHODS.
+  // Akka actors have ! ?
+
+  // prefix notation
+  // unary operators
+  val x = -1
+  val y = 1.unary_-
+  // unary_ prefix only works with - + ~ !
+  println(!mary)
+  println(mary.unary_!)
+
+  /* // postfix notation
+  println(mary.isAlive)
+  println(mary isAlive)*/ // not turn on default now
+
+  // apply
+  println(mary.apply())
+  println(mary()) // equivalent
+
+}

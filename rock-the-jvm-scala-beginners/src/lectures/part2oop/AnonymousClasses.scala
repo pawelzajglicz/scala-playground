08c6704c243c2a373fe65f59e2e038f2cb98847e
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-12-27 13:36:07 +0100

    anonymous classes

---
+++ rock-the-jvm-scala-beginners/src/lectures/part2oop/AnonymousClasses.scala
@@ -0,0 +1,33 @@
+package lectures.part2oop
+
+object AnonymousClasses extends App {
+
+  abstract class Animal {
+    def eat: Unit
+  }
+
+  // anonymous class
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("hahahahaha")
+  }
+
+  /*
+    equivalent with
+    class AnonymousClasses$$anon$1 extends Animal {
+      override def eat: Unit = println("hahahahaha")
+    }
+    val funnyAnimal = new AnonymousClasses$$anon$1
+   */
+
+  println(funnyAnimal.getClass)
+
+  class Person(name: String) {
+    def sayHi: Unit = println(s"Hi, my name is $name")
+  }
+
+  val jim = new Person("Jim") {
+    override def sayHi: Unit = println(s"Hi, my name is Jim, how can I be your service?")
+  }
+
+  jim.sayHi
+}

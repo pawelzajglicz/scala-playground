77546ff6084f1074c7ff8a4c836637e0cf67e10a
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-12-27 11:33:04 +0100

    generics

---
+++ rock-the-jvm-scala-beginners/src/lectures/part2oop/Generics.scala
@@ -0,0 +1,57 @@
+package lectures.part2oop
+
+object Generics extends App {
+
+
+  trait test[A]
+
+  class MyList[+A] {
+    // use the type A
+    def add[B >: A](element: B): MyList[B] = ???
+    /*
+      A = Cat
+      B = Dog = Animal
+     */
+  }
+
+  class MyMap[Key, Value]
+
+  val listOfIntegers = new MyList[Int]
+  val listOfStrings = new MyList[String]
+
+  // generic methods
+  object MyList {
+    def empty[A]: MyList[A] = new MyList[A]
+  }
+
+  val emptyListOfIntegers = MyList.empty[Int]
+
+  // variance problem
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  // 1. yes List[Cat] extends List[Animal] = COVARIANCE
+  class CovariantList[+A]
+  val animal: Animal = new Cat
+  val animalList: CovariantList[Animal] = new CovariantList[Cat]
+  // animalList.add(new Dog) ??? HARD QUESTION  => we return a list of animals
+
+  // 2. no = INVARIANCE
+  class InvariantList[A]
+  // not allowed val invariantAnimalList: InvariantList[Animal] = new InvariantList[Cat]
+
+  // 3. Hell, no! CONTRAVARIANCE
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
+
+  // bounded types
+  class Cage[A <: Animal](animal: A)
+  val cage = new Cage(new Dog)
+
+  /* not allowed class Car
+  val cageCar = new Cage(new Car)*/
+
+
+  // expand MyList to be generic
+}

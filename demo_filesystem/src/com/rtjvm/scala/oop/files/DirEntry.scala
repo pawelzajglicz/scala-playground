1322f27f624a27e03f2722fdc85251997395a57f
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-01-13 18:44:35 +0100

    mkdir command

--- demo_filesystem/src/com/rtjvm/scala/oop/files/DirEntry.scala
+++ demo_filesystem/src/com/rtjvm/scala/oop/files/DirEntry.scala
@@ -2,4 +2,7 @@ package com.rtjvm.scala.oop.files
 
 abstract class DirEntry(val parentPath: String, val name: String) {
 
+  def path: String = parentPath + Directory.SEPARATOR + name
+
+  def asDirectory: Directory
 }
--- demo_filesystem/src/com/rtjvm/scala/oop/files/Directory.scala
+++ demo_filesystem/src/com/rtjvm/scala/oop/files/Directory.scala
@@ -3,11 +3,26 @@ package com.rtjvm.scala.oop.files
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
 
+  def hasEntry(name: String): Boolean = ???
+
+  def getAllFoldersInPath: List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList
+
+  def findDescendant(path: List[String]): Directory = ???
+
+  def addEntry(newEntry: DirEntry): Directory = ???
+
+  def findEntry(entryName: String): DirEntry = ???
+
+  def replaceEntry(entryName: String, newEntry: DirEntry): Directory = ???
+
+  override def asDirectory: Directory = this
+
 }
 
 object Directory {
   val SEPARATOR = "/"
-  val ROOT_PAH ="/"
+  val ROOT_PATH ="/"
 
   def ROOT: Directory = Directory.empty("", "")
 
